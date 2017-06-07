fd1055f0f9ded0e0e34c60f7b2391683d29e567a
Jim Wharton <jimwharton@overstock.com>
2017-06-07 11:18:04 -0600

    Mutate copy instead of original

--- src/organize.js
+++ src/organize.js
@@ -1,9 +1,9 @@
 function organize(regions, rules) {
     // Make a copy so we can check inclusion
-    const rulesOriginal = rules.slice();
+    const rulesCopy = rules.slice();
 
     return regions.map(region => {
-        return rulesOriginal.includes(region) ? rules.shift() : region;
+        return rules.includes(region) ? rulesCopy.shift() : region;
     });
 }
 
